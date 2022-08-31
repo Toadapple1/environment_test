ba8f21c492f08d0e5c9b8a86db4e4d4e7b8eab49
Toadapple1 <71990502+Toadapple1@users.noreply.github.com>
2022-08-31 16:56:55 -0500

    Added the Book title test

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+   test "no title" do 
+     book = Book.new 
+     assert_not book.save, "There is no Title"
+   end
+   test "errors?" do 
+     assert_raises(NameError) do
+       some_undefined_variable
+     end
+    end
 end
