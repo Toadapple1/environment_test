a8479dd0041016b31535d6e30f96f477ba3b8bb8
Toadapple1 <71990502+Toadapple1@users.noreply.github.com>
2022-09-15 15:50:13 -0500

    redid integration and unit testing for screenshots and rerouted all to homepage

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,14 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'Nam Cao'
+    fill_in 'Price', with: 23.0
+    fill_in 'Published date', with: '2025-09-15 15:44:38'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('Nam Cao')
+    expect(page).to have_content(23.0)
+    expect(page).to have_content('2025-09-15 15:44:38 UTC')
   end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,12 +1,9 @@
 require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
-  test "There might be no titles" do 
-    book = Book.new
-    asser_not book.save, "There is no title for the book"
-  end
+  # test "the truth" do
+  #   assert true
+  # end
+
 
 end
